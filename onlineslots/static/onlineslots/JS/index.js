2558a3c53a2409a7282b503b4089fa27ec19fa3d
lioraiz <lior.aizenberg@gmail.com>
2017-08-24 11:54:41 +0300

    Commented

--- onlineslots/static/onlineslots/JS/index.js
+++ onlineslots/static/onlineslots/JS/index.js
@@ -1,25 +1,31 @@
 $(document).ready(function() {
+// Hide the div appearing after user runs out of credit
 $(".reset").click(function() {
     $(".endgame").css("display", "none");
 })
+
 $(".play").click(function() {
     var suits = ["club", "diamond", "heart", "spade"];
     var result = "";
+    // Spin the reels
     if ($("reel").attr("class") != "spin"){
         $("reel").toggleClass("spin").removeClass("club diamond heart spade");
         $(".cashout").removeClass("win");
     }
     else {
+    // Stop the reels
         $("reel").toggleClass("spin");
         $("reel").each(function() {
             $(this).toggleClass(suits[Math.floor(Math.random() * suits.length)]);
         });
+        // Round outcome
         if (($("#reel1").attr("class")) == ($("#reel2").attr("class")) && ($("#reel1").attr("class")) == ($("#reel3").attr("class"))){
             $(".cashout").addClass("win");
             var result = "win"
         } else {
             var result = "lose"
         }
+        // Update credit using ajax
         $.ajax({
                 type:"POST",
                 url:"/gameresult/",
@@ -33,12 +39,12 @@ $(".play").click(function() {
 
                     $('.funds').text(details.userfund)
                     $('.endgame').hide()
-
+                    // Out of credit
                     if (!details.restart == "") {
                         $('.endgame').show()
                     }
                 }
-            })
+       })
     }                              
 });
 });

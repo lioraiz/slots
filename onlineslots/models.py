7a08831c48a5aae298994a2abc2fce8dce8d235e
lioraiz <lior.aizenberg@gmail.com>
2017-08-24 11:58:05 +0300

    Commented

--- onlineslots/models.py
+++ onlineslots/models.py
@@ -1,6 +1,7 @@
 from django.contrib.auth.models import User
 from django.db import models
 
+# The funds table
 class Funds(models.Model):
     player = models.OneToOneField(User, on_delete=models.CASCADE)
     amount = models.IntegerField(default=100)
--- onlineslots/views.py
+++ onlineslots/views.py
@@ -1,25 +1,23 @@
 import json
-
 from django.contrib import auth
-from django.shortcuts import render, render_to_response
-from django.http import Http404, HttpResponseRedirect, HttpResponse
+from django.shortcuts import render_to_response
+from django.http import HttpResponseRedirect, HttpResponse
 from django.template.context_processors import csrf
-
-from .models import Funds
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
-from django.views.generic import View
 from .forms import UserForm
+from .models import Funds
 
-
-
+# Main game mechanism
 def game(request):
+    # Conncet funds to user
     activeuser = request.user
     userfunds = Funds.objects.get(player=activeuser)
     if request.method == "POST":
         context = {}
         gameresult = request.POST.get('result')
         print(userfunds.player)
+        # Handle round outcome
         if gameresult == 'lose':
             userfunds.amount -= 5
             context["userfund"] = userfunds.amount
@@ -31,21 +29,17 @@ def game(request):
             context["restart"] = ""
             userfunds.save()
 
+        # Handle out of credit - reset funds
         if userfunds.amount <= 0:
-            context["restart"] = "its a fake game so keep going on!"
+            context["restart"] = "You're out of credit... let's get you sorted!"
             userfunds.amount = 100
             userfunds.save()
-
-
-
-
         return HttpResponse(json.dumps(context))
 
-
     else:
-
         return render(request, 'onlineslots/game.html', {"userfunds":userfunds})
 
+# User generation
 def UserFormView(request):
     if request.method == "POST":
         form = UserForm(request.POST)
@@ -61,29 +55,28 @@ def UserFormView(request):
         form = UserForm()
         return render(request, "onlineslots/registration_form.html", {"form":form})
 
-
+# Login page
 def login_in(request):
     c = {}
     c.update(csrf(request))
     return render_to_response("onlineslots/index.html", c)
 
-
+# User authentication
 def auth_view(request):
     username = request.POST.get("username", "")
     password = request.POST.get("password", "")
     user = auth.authenticate(username=username, password=password)
-
     if user is not None:
         auth.login(request, user)
         return HttpResponseRedirect("/game/")
     else:
         return HttpResponseRedirect("/invalid_login/")
 
-
+# Invalid user
 def invalid_login(request):
     return render_to_response("onlineslots/invalid_login.html")
 
-
+# User logout
 def logout(request):
     auth.logout(request)
     return render_to_response("onlineslots/logged_out.html")
